476ad247fd955c5294400b6a658f98817a902ccc
HITECH BOTS <70721362+kjeymax@users.noreply.github.com>
2021-07-31 00:20:28 +0530

    Update config.py

--- mrjoker/config.py
+++ mrjoker/config.py
@@ -14,9 +14,9 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID= 1383845  # integer value, dont use ""
-    API_HASH= "0e3d2c299cc3c5cc26c283cecd2eb97c"
-    TOKEN = "1384019653:AAGeq9rGVQqlTcP45sWZDhW2oEQ2YZ8bmGs"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    API_ID= 126456 # integer value, dont use ""
+    API_HASH= "0"
+    TOKEN = "0"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1131653685   # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "kavinduaj"
     SUPPORT_CHAT = "lkhitech"  # Your own group for support, do not add the @
@@ -28,14 +28,14 @@ class Config(object):
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "postgres://qyspwzpivfghzr:c37ed8e85c87c1f1c4dfbe2b56587ed5c64c5e68d4ef4c407657c94008d9e576@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d72ao2ttmnpaom"  
+    SQLALCHEMY_DATABASE_URI = ""  
     REDIS_URI = " "
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = "DFOm~tAD3jGW_l_pxisfl1FETDTytFaz831fAciaKZg9kCoAYuPAP0lYSO5QDdbC"  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
@@ -59,9 +59,9 @@ class Config(object):
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        "GFO4UK7F4NGUMYTT"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "1AW4Q9DTFF4"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = ""  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "xyz"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
