9819451ef1971ba5139538285d1ccc32f0141a28
HITECH BOTS <70721362+kjeymax@users.noreply.github.com>
2021-07-31 00:27:56 +0530

    Add files via upload

---
+++ mrjoker/modules/animequotes.py
@@ -0,0 +1,19 @@
+
+import random
+
+from telegram import Update
+from telegram.ext import CallbackContext, run_async
+
+import mrjoker.modules.animequotesstring as animequotesstring
+from mrjoker import dispatcher
+from mrjoker.modules.disable import DisableAbleCommandHandler
+
+
+@run_async
+def animequotes(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(random.choice(animequotesstring.ANIMEQUOTES))
+
+
+ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("animequotes", animequotes)
+
+dispatcher.add_handler(ANIMEQUOTES_HANDLER)
---
+++ mrjoker/modules/yt.py
@@ -0,0 +1,5 @@
+__help__ = """
+ 🔹 - /yt <text>: perform a youtube search
+ 🔹 - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+"""
+__mod_name__ = "Youtube 🖥"
