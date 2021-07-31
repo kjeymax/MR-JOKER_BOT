fa2fd32c2a618eefae2e35975a4d305aa08b9dfa
HITECH BOTS <70721362+kjeymax@users.noreply.github.com>
2021-07-31 12:53:57 +0530

    Update notes.py

--- mrjoker/modules/notes.py
+++ mrjoker/modules/notes.py
@@ -516,30 +516,32 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- 🍃 ✪ /get <notename>*:* get the note with this notename
- 🍃 ✪ #<notename>*:* same as /get
- 🍃 ✪ /notes or /saved*:* list all saved notes in this chat
- 🍃 ✪ /number *:* Will pull the note of that number in the list
+ 🔹 /get <notename>*:* get the note with this notename
+          #<notename>*:* same as /get
+ 🔹 /notes or /saved*:* list all saved notes in this chat
+ 🔹 /number *:* Will pull the note of that number in the list
 If you would like to retrieve the contents of a note without any formatting, use `/get <notename> noformat`. This can \
 be useful when updating a current note
 
 *Admins only:*
 
- 🍃 ✪ /save <notename> <notedata>*:* saves notedata as a note with name notename
+ 🔹 /save <notename> <notedata>*:* saves notedata as a note with name notename
 A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a \
 `buttonurl:` section, as such: `[somelink](buttonurl:example.com)`. Check `/markdownhelp` for more info
 
- 🍃 ✪ /save <notename>*:* save the replied message as a note with name notename
+ 🔹 /save <notename>*:* save the replied message as a note with name notename
  Separate diff replies by `%%%` to get random notes
  *Example:* 
+ 
  `/save notename
  Reply 1
  %%%
  Reply 2
  %%%
  Reply 3`
- ✪ /clear <notename>*:* clear note with this name
- ✪ /removeallnotes*:* removes all notes from the group
+ 
+ 🔹 /clear <notename>*:* clear note with this name
+ 🔹 /removeallnotes*:* removes all notes from the group
  *Note:* Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.
 
 """
