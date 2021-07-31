347b6ec7a68b87e055b83d5781fa8c7ff26388dd
HITECH BOTS <70721362+kjeymax@users.noreply.github.com>
2021-07-31 12:54:45 +0530

    Update purge.py

--- mrjoker/modules/purge.py
+++ mrjoker/modules/purge.py
@@ -98,9 +98,9 @@ async def delete_messages(event):
 __help__ = """
 *Admin only:*
 
- 🍃 ✪ /del*:* deletes the message you replied to.
- 🍃 ✪ /purge*:* deletes all messages between this and the replied to message.
- 🍃 ✪ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
+ 🔹 /del*:* deletes the message you replied to.
+ 🔹 /purge*:* deletes all messages between this and the replied to message.
+ 🔹 /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
 """
 
 __mod_name__ = "Purges 🚿"
