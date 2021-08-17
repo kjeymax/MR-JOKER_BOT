f69ff82e7e89b08b4e9e91d29c87cba1f484cc75
HITECH BOTS <70721362+kjeymax@users.noreply.github.com>
2021-08-17 16:03:26 +0530

    Update imgeditor.py

--- mrjoker/modules/imgeditor.py
+++ mrjoker/modules/imgeditor.py
@@ -445,7 +445,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await removebg_sticker(client, query.message)
 
 
-__mod_name__ = "Image Editor"
+__mod_name__ = "Image Editor 🏞"
 __help__ = """
 Cutiepii have some advanced image editing tools inbuilt
 Bright, Circle, RemBG, Blur, Border, Flip, Glitch, Sticker maker and more
