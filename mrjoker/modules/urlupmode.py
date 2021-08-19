9b896aef1a31b77bd23aa8eb91b47788ba35a323
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2021-08-19 22:19:11 +0530

    add urlupmode.py

---
+++ mrjoker/modules/urlupmode.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Upload 📤"
+
+__help__ = """
+Here is the help for the Upload module:
+Link To File:
+ 🔹 `/up`*:*  reply to a direct download link to upload it to telegram as files
+ 
+File To Link:
+ 🔹 `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link
+ """
