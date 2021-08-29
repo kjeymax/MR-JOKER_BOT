c27a3d90414f76e0a75d6d383189e96f9d9b2af6
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2021-08-29 22:28:16 +0530

    Delete mrjoker directory

--- mrjoker/modules/yt.py
+++
@@ -1,5 +0,0 @@
-__help__ = """
- 🔹 - /yt <text>: perform a youtube search
- 🔹 - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
-"""
-__mod_name__ = "Youtube 🖥"
