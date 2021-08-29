291c2d345e0113c880c04961c4ef4bcf60d99a94
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2021-08-29 22:43:13 +0530

    🤡

---
+++ mrjoker/modules/moretool.py
@@ -0,0 +1,35 @@
+__help__ = """
+** GPS **
+
+ 🔹 `/gps` <Place> : Show Location on a map
+ 
+ ** Blue Cleaner **
+
+ 🔹 `/cleanblue on` : Turn bluetext cleaner on
+ 🔹 `/cleanblue off` : Turn bluetext cleaner off
+ 
+ ** Send **
+
+ 🔹 `/snd` <Text> : Message like the bot
+ 
+** Grammer **
+
+ 🔹 `/t` <reply> : Show grammer corrected text
+ 
+** Image Tools**
+
+ 🔹 `/img` <text>: perform a image search
+ 🔹 `/getqr` <reply to image>: Read QR code
+ 🔹 `/makeqr` <reply to text>: Make QR code
+ 
+** Style Text **
+
+ 🔹 `/weebify` : Weebify Text
+ 🔹 `/square` : square Text
+ 🔹 `/blue` : Blues text
+ 
+** More **
+
+ 🔹 `/phone` <tp no> : Track Phone no
+"""
+__mod_name__ = "More Tools"
