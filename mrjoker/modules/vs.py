c5de7463263029b519b0bb67fe0f4fc62e284289
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2021-09-28 17:31:08 +0530

    Create vs.py

---
+++ mrjoker/modules/vs.py
@@ -0,0 +1,24 @@
+__help__ = """
+*All Command List:*
+🔹 `/vplay` (reply to video or yt/live url) - to stream video
+🔹 `/vstop ` - stop the video streaming
+🔹 `/jvsong` (song name) - download song from YT
+🔹 `/vsong` (video name) - download video from YT
+🔹 `/lyric` (song name) - lyric scrapper
+🔹 `/vjoin` - invite assistant join to your group
+🔹 `/vleave` - order assistant leave from your group
+*SUDO ONLY:*
+🔹 `/rmdl` - remove all downloaded files
+🔹 `/wipe` - remove all downloaded raw files
+🔹 `/leaveall` - order assistant leave from all group
+*HOW TO USE THIS BOT:*
+1.) first, add me to your group.
+2.) then promote me as admin and give all permissions except anonymous admin.
+3.) add @Mrjokermusicbo to your group.
+4.) turn on the voice chat first before start to stream video.
+5.) type /vplay (reply to video) to start streaming.
+6.) type /vstop to end the video streaming.
+💡 *Note: stream & stop command can only be executed by group admin only!*
+"""
+
+__mod_name__ = "Video Stream"
