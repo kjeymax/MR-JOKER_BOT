46fa9327a83079bf6b99ed70f4191e890372eb40
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2021-10-22 05:12:32 -0700

    Create chatbots.py

---
+++ mrjoker/modules/chatbots.py
@@ -0,0 +1,12 @@
+🤡 *Mr.Joker Luna AI Chatbot* 
+
+ 🔹 `/chatbot` on/off: Enables and disables Luna AI Chat mode (EXCLUSIVE)
+ 🔹 `/chatbot en `: Enables English only chatbot
+ 
+*Powered by* : [Luna ChatBot](https://github.com/TheHamkerCat/LunaChatBot)
+
+🤡 *Mr.Joker Kuki AI Chatbot* 
+
+🔹 `/addkuki `: Enables and Disables Kuki AI Chat mode (EXCLUSIVE)
+
+*Powered by* : [Kuki Chatbot](https://github.com/MoeZilla/KukiChatbot)
