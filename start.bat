98027322c28b83dfc78998715f67b1827d2ea9dd
CHIRAG84 <94229683+Zacky1239@users.noreply.github.com>
2021-11-23 07:43:34 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE MR JOKER BOT
+TITLE RSN-BOT
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m mrjoker
+env\scripts\activate.bat && py -m MR CHIRAG
